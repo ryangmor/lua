4967c8511d1c239dd26c7062cacde10197742875
ryangmor <ryangmor@gmail.com>
2016-04-07 19:27:41 +0430

    Added files via upload

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ import json
 import time
 from pprint import pprint
 
-API_TOKEN = '201987057:AAHC8i8OI-0EfCc4bt_lFvU3Gk0edypRtLA'
+API_TOKEN = '128124618:AAFA_V5A5xyJ_09HNFIH_SFRenRhTI2JIyM'
 bot = telebot.TeleBot(API_TOKEN)
 
 with open('loggingids.json') as f:
@@ -34,50 +34,40 @@ def on_user_joins(m):
 				user.append(str(cid))
 				with open('chat.txt', 'a') as f:
 					f.write(str(cid)+"\n")
-			bot.send_message(cid, "Hi Thanks for invite me but Im Not A bot For Groups Check Me ON Pv ")
+			bot.send_message(cid, " ")
 			print "New group received."
 			userwhogotadded = m.new_chat_participant.first_name
 			username = m.new_chat_participant.username
 			groupname = m.chat.title
 			groupid = m.chat.id
-			bot.send_message('-141680767', "# DEBUG # " + "Bot got invited to the group " + str(groupname) + "(" + str(groupid) + ")", parse_mode="HTML")
+			bot.send_message('52038630', "# DEBUG # " + "Bot got invited to the group " + str(groupname) + "(" + str(groupid) + ")", parse_mode="HTML")
 
 
 
 
-@bot.message_handler(commands=['admins'])
-def admins(m):
+@bot.message_handler(commands=['credits'])
+def credits(m):
 	cid = m.chat.id
-	bot.send_message(cid, "@RyanGmor And @Programing_soft Are Our Admins", parse_mode="Markdown")
-
-@bot.message_handler(commands=['ch'])
-def ch(m):
-	cid = m.chat.id
-	bot.send_message(cid, "Our Public Channel IS : @Cactus_Channel", parse_mode="Markdown")
-	
-@bot.message_handler(commands=['gplink'])
-def gplink(m):
-	cid = m.chat.id
-	bot.send_message(cid, "Sorry We Didnt Start Yet For Making A Gp :( ", parse_mode="Markdown")
+	bot.send_message(cid, "@rrrrr", parse_mode="Markdown")
 
 @bot.message_handler(commands=['help'])
 def help(m):
 	cid = m.chat.id
-	bot.send_message(cid, "Use /q To Ask A Question. Admins Will Receive This Question  \nIf you want to see Commands you can use : /help \n if You Wants To See Who Is Our Team Admin : /admins \n For Getting an invite link to our Support Team Use : /gplink \n For Know Our Channel Use : /ch ", parse_mode="Markdown")
+	bot.send_message(cid, "use /ask <MSG> for ask your question", parse_mode="Markdown")
 	
-@bot.message_handler(commands=['q'])
-def q(m):
+@bot.message_handler(commands=[''])
+def ask(m):
     tmt = m.from_user.id
     idA, cid = m.chat.id, m.chat.id
     str = m.text
-    txt = str.replace("/q", "")
-    bot.send_message(idA, "<b>So Our Admins Will Receive This Question ! your Question:\n Quetsion:</b> {} \n<b>Chat-Id :</b> {} \n<b>We will answer you as soon as possible</b>".format(txt,idA), parse_mode="HTML")
-    bot.send_message('-141680767', "<b>#NEW MSG\n Chat-Id:</b> {} \n<b>Msg :</b> {}".format(idA,txt), parse_mode="HTML")
-    bot.send_message(idA, "<b>Deliverd</b>", parse_mode="HTML")
+    txt = str.replace("", "")
+    bot.send_message(idA, "<b>Thank you very much! Details to your Question:\n Quetsion:</b> {} \n<b>Chat-Id :</b> {} \n<b>We will answer you as soon as possible</b>".format(txt,idA), parse_mode="HTML")
+    bot.send_message('52038630', "<b>#NEW MSG\n Chat-Id:</b> {} \n<b>Msg :</b> {}".format(idA,txt), parse_mode="HTML")
+    bot.send_message(idA, "<b>This is a confirmation that we received your Question</b>", parse_mode="HTML")
 
 
-@bot.message_handler(commands=['a'])
-def a(m):
+@bot.message_handler(commands=['answer'])
+def answer(m):
         datafile = [line.rstrip('\n') for line in open('admins.txt','rt')]
         tmt = m.from_user.id
         idA, cid = m.chat.id, m.chat.id
@@ -89,7 +79,7 @@ def a(m):
         txt = m.text.split()[2:]
         text = ' '.join(txt)
         from_id = m.from_user.first_name
-        bot.send_message(to_id, "<b>Admin:</b> <i>{}</i> <b>has answered your question:</b>\n <i>{}</i>".format(from_id,text), parse_mode="HTML")
+        bot.send_message(to_id, "<b>Staff</b> <i>{}</i> <b>has answered your question:</b>\n <i>{}</i>".format(from_id,text), parse_mode="HTML")
      
      
 @bot.message_handler(commands=['bb'])
@@ -151,7 +141,7 @@ def welcome(m):
 				with open('user.txt', 'a') as f:
 					f.write(str(id)+"\n")
 		                print "New user received."
-	                        bot.send_message(id, "Use /q To Ask A Question. Admins Will Receive This Question  \nIf you want to see Commands you can use : /help \n if You Wants To See Who Is Our Team Admin : /admins \n For Getting an invite link to our Support Team Use : /gplink \n For Know Our Channel Use : /ch ", parse_mode="Markdown")
+	                        bot.send_message(id, "use /ask <MSG> for ask your question", parse_mode="Markdown")
         return
 @bot.message_handler(commands=['stats'])
 def check(m):
@@ -187,10 +177,10 @@ def id(m):
     cid = m.chat.id
     uid = m.from_user.id
     if cid > 0:
-        bot.send_message( cid, "Helloooo %s , Your ID is %s.\n" %(m.from_user.first_name, cid))
+        bot.send_message( cid, "Hi %s , Your ID is %s.\n" %(m.from_user.first_name, cid))
         print "ID command received"
     else:
-        bot.send_message( cid, "Hi Guys %s , The group ID is %s.\n" %(m.from_user.first_name, cid))
+        bot.send_message( cid, "Hi %s , The group ID is %s.\n" %(m.from_user.first_name, cid))
         print "ID command received"
 
 		
